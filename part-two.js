3b54bcf9bdbd1d90c53f9650d2ce013332cc6005
aung-st <s.taung1445@gmail.com>
2025-02-28 20:21:41 +0000

    feat: complete part 2 minus a bug

--- part-two.js
+++ part-two.js
@@ -1,19 +1,36 @@
 const buttons = document.querySelectorAll(".dial-pad-button");
 let code = ""
-solved = False 
+let solved = false; 
 
-buttons.forEach( button => button.addEventListener("click", (e) => {
-    if (solved){
-        goToNextPage();
-    }   else{
+// TODO: check that the puzzle is solved without having to press a button otherwise if you get 159 then you have 
+// to press 1 more time to get the next button to show
+if (solved){
+    goToNextPage();
+    } else {
+        // Every time a button is pressed the code string should be updated
+        buttons.forEach( button => button.addEventListener("click", (e) => {
+            trackCode();
             const clickedPad = e.currentTarget.getAttribute("data-number");
-            code += clickedPad;      
-        }      
-}));
+            code += clickedPad; 
+            displayCode();     
+        }));
+    }
 
+function displayCode(){
+    document.getElementById("code").innerHTML = code;
+}
+
+// reset code string 
+function reset(){
+    code = "";
+    displayCode();
+}
+
+// check if code is correct and if so show the next button
 function trackCode(){
-    if (code == "1111"){
+    if (code == "159"){
         document.getElementById("next-button").style.visibility="visible";
+        solved = true;
     }
 }
 
