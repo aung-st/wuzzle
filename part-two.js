7d59abe1b296c78e225c4b65437511879d72db5f
aung-st <s.taung1445@gmail.com>
2025-02-28 19:12:46 +0000

    feat: add wuzzle part two draft without much styling

--- part-two.js
+++ part-two.js
@@ -1,10 +1,22 @@
 const buttons = document.querySelectorAll(".dial-pad-button");
 let code = ""
+solved = False 
 
 buttons.forEach( button => button.addEventListener("click", (e) => {
-        const clickedPad = e.currentTarget.getAttribute("data-number");
-        code += clickedPad;
-        console.log(clickedPad);
-        console.log(code);
+    if (solved){
+        goToNextPage();
+    }   else{
+            const clickedPad = e.currentTarget.getAttribute("data-number");
+            code += clickedPad;      
+        }      
+}));
+
+function trackCode(){
+    if (code == "1111"){
+        document.getElementById("next-button").style.visibility="visible";
     }
-))
+}
+
+function goToNextPage(){
+    window.location.href = ("part-three.html");
+}
