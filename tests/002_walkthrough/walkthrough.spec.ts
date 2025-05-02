be2383638c0a2028405c354db020de253d69f634
aung-st <s.taung1445@gmail.com>
2025-05-02 13:11:38 +0100

    feat: finish walkthrough

--- tests/002_walkthrough/walkthrough.spec.ts
+++ tests/002_walkthrough/walkthrough.spec.ts
@@ -35,5 +35,71 @@ test("walkthrough", async ({ page,baseURL }) => {
   await sleep(1000);
   await page.getByRole('button', { name: /Next Part/i }).click();
 
+  // part 3
+  await expect(page.locator(".modal-button-1")).toBeVisible();
+  await page.locator(".modal-button-1").click();
+
+  await expect(page.locator(".close-button-1 > i:nth-child(1)")).toBeVisible();
+  await page.locator(".close-button-1 > i:nth-child(1)").click();
+
+  await expect(page.locator("#answer")).toBeVisible();
+  await page.locator("#answer").click();
+
+  await page.getByRole('textbox').fill('One');
+  await sleep(1000);
+  await page.getByRole('button', { name: /Submit/i }).click();
+  await sleep(1000);
+  await page.getByRole('button', { name: /Next Part/i }).click();
+
+  // part 4 
+  await expect(page.locator("#dial-one")).toBeVisible();
+  await page.locator("#dial-one").click();
+  await page.locator("#dial-one").click();
+  await page.locator("#dial-one").click();
+  await sleep(2000);
+
+
+  await expect(page.locator("#dial-two")).toBeVisible();
+  await page.locator("#dial-two").click();
+  await page.locator("#dial-two").click();
+  await page.locator("#dial-two").click();
+  await page.locator("#dial-two").click();
+  await page.locator("#dial-two").click();
+  await page.locator("#dial-two").click();
+  await page.locator("#dial-two").click();
+  await sleep(2000);
+
+  await expect(page.locator("#dial-three")).toBeVisible();
+  await page.locator("#dial-three").click();
+  await page.locator("#dial-three").click();
+  await page.locator("#dial-three").click();
+  await page.locator("#dial-three").click();
+  await page.locator("#dial-three").click();
+  await sleep(2000);
+
+  await expect(page.locator("#dial-four")).toBeVisible();
+  await page.locator("#dial-four").click();
+  await page.locator("#dial-four").click();
+  await sleep(2000);
+  await page.getByRole('button', { name: /Next Part/i }).click();
+
+  // part last
+  await expect(page.locator("div.eye:nth-child(1)")).toBeVisible();
+  await page.locator("div.eye:nth-child(1)").click();
+  await page.locator("div.eye:nth-child(1)").click();
+  await page.locator("div.eye:nth-child(1)").click();
+  await page.locator("div.eye:nth-child(1)").click();
+  await page.locator("div.eye:nth-child(1)").click();
+  await page.locator("div.eye:nth-child(1)").click();
+  await page.locator("div.eye:nth-child(1)").click();
+  await page.locator("div.eye:nth-child(1)").click();
+  await page.locator("div.eye:nth-child(1)").click();
+  await page.locator("div.eye:nth-child(1)").click();
+
+
+  await page.locator("#next-button").click();
   await sleep(3000);
+
+
 });
+
